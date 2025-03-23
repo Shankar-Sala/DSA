e700267d8cd53e478b3b4593aa0776ed0c28a12c
Shankar-Sala <shankarsala9995@gmail.com>
2025-03-23 18:31:27 +0530

    first commit

--- 1) Variables & Datatypes/area.java
+++ 1) Variables & Datatypes/area.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class areaOfCircle{
+public class area{
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
     float rad =sc.nextFloat();
